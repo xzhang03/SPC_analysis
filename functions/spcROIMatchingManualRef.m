2b0fdf7360d215e3f63af6418e69e6d1188dd2ee
Stephen Zhang <x.stephen.zhang@gmail.com>
2020-06-05 15:28:02 -0400

    Cross day alignment mode

--- functions/spcROIMatchingManualRef.m
+++ functions/spcROIMatchingManualRef.m
@@ -1,6 +1,6 @@
-function spcROIMatchingManual(mouse, date, runs, varargin)
-% spcROImatching matches ROIs between experiments, using the first
-% experiment as the main template. This function requires manual clicking.
+function spcROIMatchingManualRef(mouse, date, runs, varargin)
+% spcROImatchingRef matches ROIs between experiments, using another experiment
+% as the main template. This function requires manual clicking.
 
 %% Parse inputs
 p = inputParser;
@@ -19,6 +19,12 @@ addOptional(p, 'redalpha', 0.5);
 addOptional(p, 'multifov', false);
 addOptional(p, 'fov', 1); % Specify fov
 
+% Reference info
+addOptional(p, 'refmouse', '');
+addOptional(p, 'refdate', '');
+addOptional(p, 'refrun', 1);
+addOptional(p, 'reffov', []);
+
 % Unpack if needed
 if iscell(varargin) && size(varargin,1) * size(varargin,2) == 1
     varargin = varargin{:};
@@ -30,8 +36,23 @@ p = p.Results;
 %% Clean up inputs
 % Case and type
 mouse = upper(mouse);
+
+if isempty(p.refmouse)
+    p.refmouse = mouse;
+end
+
+p.refmouse = upper(p.refmouse);
+
 if ~ischar(date)
     date = num2str(date);
+end
+
+if ~ischar(p.refdate)
+    p.refdate = num2str(p.refdate);
+end
+
+if isempty(p.reffov)
+    p.reffov = p.fov;
 end
 
 % User (add yourself if needed)
@@ -101,30 +122,61 @@ for i = 1 : length(runs)
     RGB_cell{i} = RGB;
 end
 
+%% IO reference
+% Get paths
+refpath = spcPath(p.refmouse, p.refdate, p.refrun, 'server', p.server, 'user', p.user,...
+    'slice', p.slice, 'cdigit', p.cdigit, 'multifov', p.multifov, 'fov', p.reffov);
+
+% Indices for cells
+cellref = load(fullfile(refpath.fp_out, refpath.xrun_mat), 'ROI_cell_clean');
+cellref = cellref.ROI_cell_clean;
+refinds = [cellref{:,1}];
+
+% Refence image
+imref = imread(fullfile(refpath.fp_out, refpath.ROI_ref));
+
+% Get reference border
+refbox = regionprops(imref(:,:,3) < 100, 'BoundingBox');
+refbox = round(refbox.BoundingBox);
+
+% Apply reference border
+imref = imref(refbox(2) : refbox(2) + refbox(4), refbox(1) : refbox(1) + refbox(3), :); 
+
 %% Intial plot
 % Cells to get all the handles
 hfigs = cell(length(runs),1);
 hrgb = cell(length(runs),1);
 
 % ROI cell
-ROI_cell = cell(ROI_struct(1).nROIs, length(runs));
-ROI_cell(:,1) = num2cell(1 : ROI_struct(1).nROIs);
+ROI_cell = cell(length(refinds), length(runs) + 1);
+ROI_cell(:,1) = num2cell(refinds);
+
+% Make reference figure
+reffig = figure('name', sprintf('Reference'));
+imshow(imref);
+reftitle = title('Reference');
+reftitle.Position(2) = 2;
+set(reffig, 'Position', figpos(1,:));
+
 
 for i = 1 : length(runs)
     % Figure
-    hfigs{i} = figure('Position', figpos(i,:), 'name', sprintf('Run %i', runs(i)));
+    hfigs{i} = figure('Position', figpos(i+1,:), 'name', sprintf('Run %i', runs(i)));
     hrgb{i} = imagesc(RGB_cell{i});
     title(sprintf('Run %i', runs(i)));
 end
 
 % Getting user input
-flagdone = false;
-while ~flagdone
+for ii = 1 : length(refinds)
+    % Reference index
+    refind = refinds(ii);
+    reftitle.String = sprintf('Find %i', refind);
+    
     % User choice
-    choice = questdlg('Selecting more ROIs?', 'User input', 'More', 'Done', 'More');
+    choice = questdlg(sprintf('Can you find %i?', refind), 'User input', 'Yes', 'No', 'Yes');
     
     switch choice
-        case 'More'
+        case 'Yes'
             % Initialize chosen vector
             chosen = zeros(length(runs),1);
             
@@ -147,21 +199,16 @@ while ~flagdone
             % Apply inputs
             for i = 1 : length(runs)
                 % Apply cross-matched ROIs
-                ROI_struct(i).ROI_xmatch(ROIs_left{i} == chosen(i)) = chosen(1);
+                ROI_struct(i).ROI_xmatch(ROIs_left{i} == chosen(i)) = refind;
                 
                 % Update cell
-                ROI_cell{chosen(1), i} = chosen(i);
+                ROI_cell{ii, i+1} = chosen(i);
             end
             
             % Remove ROI from pools
             for i = 1 : length(runs)
                 % Decraese ROI count by 1
                 nROIs_left(i) = nROIs_left(i) - 1;
-                
-                % If no more ROI left in any of the runs, it's done
-                if nROIs_left(i) == 0
-                    flagdone = true;
-                end
                 
                 % Remove ROI
                 ROIs_left{i}(ROIs_left{i} == chosen(i)) = 0;
@@ -172,15 +219,14 @@ while ~flagdone
                 % Update figure
                 hrgb{i}.CData = RGB_cell{i};
             end
-        case 'Done'
-            % Done
-            flagdone = true;
-            for i = 1 : length(runs)
-                close(hfigs{i});
-            end
     end
 end
 
+%% Close
+close(reffig);
+for i = 1 : length(runs)
+    close(hfigs{i});
+end
 
 %% Cleaning
 % Matrix that denotes when ROIs are dropped
