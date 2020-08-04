eb0842127e16bb3b109be52e86a9a68683da698f
hugofluhr <hugo.fluhr@gmail.com>
2020-08-04 10:56:45 -0400

    added sample images

--- hugo/start_script.m
+++ hugo/start_script.m
@@ -1,5 +1,5 @@
 addpath(genpath('C:/Users/Hugo/Documents/MATLAB/SPC_analysis'))
 rmpath('C:/Users/Hugo/Documents/MATLAB/SPC_analysis/hugo/archive/')
 
-sbx_path = "C:\Users\Hugo\Desktop\sbx_sample.tif";
-flim_path = "C:\Users\Hugo\Desktop\flim_sample.tif";
+sbx_path = "C:\Users\Hugo\Documents\MATLAB\SPC_analysis\hugo\sample_images\sbx_sample.tif";
+flim_path = "C:\Users\Hugo\Documents\MATLAB\SPC_analysis\hugo\sample_images\flim_sample.tif";
