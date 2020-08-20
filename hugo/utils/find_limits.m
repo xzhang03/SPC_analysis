f2da4f13f6d4ec40001f2da1e380e9220adef332
hugofluhr <hugo.fluhr@gmail.com>
2020-08-20 16:04:45 -0400

    removed useless fct

--- hugo/utils/find_limits.m
+++
@@ -1,7 +0,0 @@
-function [x1,x2,y1,y2] = find_limits(image)
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
-half_x = round(size(image,1)
-x1 = find(image(rosize(image,1)
-end
-
