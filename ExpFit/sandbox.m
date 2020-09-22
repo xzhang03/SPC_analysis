d1fe99aad574af5e3d44bd5563b602b755873711
hugofluhr <hugo.fluhr@gmail.com>
2020-09-22 11:55:10 -0400

    reading multiple sdt files

--- ExpFit/sandbox.m
+++ ExpFit/sandbox.m
@@ -1,17 +1,38 @@
 clear
-tm_path = "H:\2p\stephen\SZ334\200624_SZ334\FLIM\200624_SZ334_run1\200624_SZ334_run1_c1_t1.asc";
 sdt_path = 'H:\2p\stephen\SZ334\200624_SZ334\FLIM\200624_SZ334_run1\200624_SZ334_run1_c1.sdt';
 
-im = load(tm_path);
-sdt = bfopen(sdt_path);
-
-%% Manip sdt file
-out = cat(3,sdt{1}{:,1});
-
-phot_count = squeeze(sum(sum(out,1),2));
-
+[phot_count, out] = read_sdt(sdt_path);
 %%
 figure
 plot(phot_count)
+xlabel('time bin')
+ylabel('nb of photons')
+
+%% all sdt files
+clear
+rootdir = 'H:\2p\stephen\';
+filelist = dir(fullfile(rootdir,'**/*.sdt'));
+filelist = struct2table(filelist);
+filelist = filelist(~contains(filelist.folder,'blur'),:);
+
+%% group slices and GRIN experiments
+slices = filelist(contains(filelist.folder,'slice'),:);
+grins = filelist(contains(filelist.folder,'run'),:);
+
+slice_dir = unique(slices.folder,'stable');
+
+
+%% focus on 1 imaging session and get all photon counts
+currDir = slice_dir{1};
+currFiles = slices(strcmp(slices.folder,currDir),:);
+
+parfor f = 1:size(currFiles,1)
+    photCount{f} = read_sdt(fullfile(currDir,currFiles.name{f}));
+end
+
+%% 
+acc = sum(cat(2,photCount{:}),2);
+figure
+plot(acc)
 xlabel('time bin')
 ylabel('nb of photons')
